a7b4785e96ac5a3b084ecb8504c0c7ba4a154860
DeviceArtist <deviceartist@sohu.com>
2026-04-03 23:49:11 +0800

    Update and rename claude-say.nim to claudesay.nim

---
+++ claudesay.nim
@@ -0,0 +1,18 @@
+import os
+const
+  CLAUDE_COLOR = "\x1b[38;5;217m"
+  reset = "\x1b[0m"
+  Yellow = "\x1b[33m"
+  Green = "\x1b[38;5;22m"
+  bold = "\x1b[1m"
+let text = paramStr(1)
+echo "     "
+echo Yellow & text
+echo Green
+echo " \\  "
+echo "  \\ "
+echo Yellow
+echo " ▐▛███▜▌ "
+echo "▝▜█████▛▘"
+echo "  ▘▘ ▝▝  "
+echo reset
